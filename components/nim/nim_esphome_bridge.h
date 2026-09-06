7b185ac933c9510b25887b9e20e23a3b50d9c5f2
Elijah Shaw-Rutschman <elijahr@users.noreply.github.com>
2026-09-06 01:07:52 -0500

    fix: use esphome::esp_log_printf_ namespace in bridge header

--- components/nim/nim_esphome_bridge.h
+++ components/nim/nim_esphome_bridge.h
@@ -15,19 +15,19 @@ extern "C" {
 #endif
 
 inline void nim_esp_log_i(const char *tag, const char *msg) {
-  esp_log_printf_(ESPHOME_LOG_LEVEL_INFO, tag, __LINE__, "%s", msg);
+  esphome::esp_log_printf_(ESPHOME_LOG_LEVEL_INFO, tag, __LINE__, "%s", msg);
 }
 
 inline void nim_esp_log_w(const char *tag, const char *msg) {
-  esp_log_printf_(ESPHOME_LOG_LEVEL_WARN, tag, __LINE__, "%s", msg);
+  esphome::esp_log_printf_(ESPHOME_LOG_LEVEL_WARN, tag, __LINE__, "%s", msg);
 }
 
 inline void nim_esp_log_e(const char *tag, const char *msg) {
-  esp_log_printf_(ESPHOME_LOG_LEVEL_ERROR, tag, __LINE__, "%s", msg);
+  esphome::esp_log_printf_(ESPHOME_LOG_LEVEL_ERROR, tag, __LINE__, "%s", msg);
 }
 
 inline void nim_esp_log_d(const char *tag, const char *msg) {
-  esp_log_printf_(ESPHOME_LOG_LEVEL_DEBUG, tag, __LINE__, "%s", msg);
+  esphome::esp_log_printf_(ESPHOME_LOG_LEVEL_DEBUG, tag, __LINE__, "%s", msg);
 }
 
 inline uint32_t nim_esp_millis(void) {
